cc2e3f4e38c3ed36657b2fc025901e786f2bfc3b
jiasin <w6522873@gmail.com>
2025-05-15 19:50:59 +0800

    add client_id

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
@@ -12,16 +11,16 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ThirdwebProvider
+  <ThirdwebProvider 
   desiredChainId={Sepolia}
   activeChain={Sepolia}
   // for the next version of thirdweb we have to register an API through: https://thirdweb.com/dashboard/settings/api-keys
-  // clientId="your-client-id"
-  >
+  clientId="48c52747f331257dad6db6b16f69b439"
+  > 
     <Router>
       <StateContextProvider>
         <App />
       </StateContextProvider>
     </Router>
-  </ThirdwebProvider>
+  </ThirdwebProvider> 
 )
