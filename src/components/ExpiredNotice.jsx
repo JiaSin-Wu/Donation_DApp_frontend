6a25ff873e28249ea88d288f39b8493f9545e98f
jiasin <w6522873@gmail.com>
2025-05-27 14:10:14 +0800

    Updates

--- src/components/ExpiredNotice.jsx
+++
@@ -1,8 +0,0 @@
-const ExpiredNotice = () => {
-  return (
-    <div className="expired-notice">
-      <span>Expired!!!</span>
-      <p>Please select a future date and time.</p>
-    </div>
-  );
-};
